a47c398d176452e63e8004eaa317249ed656eb26
arunikax <srarfian@gmail.com>
2024-08-01 00:52:30 +0700

    feat: add navBar section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/navbar/Navbar';
+import Footer from '@/components/footer/Footer';
+import { ThemeContextProvider } from '@/context/ThemeContext';
+import ThemeProvider from '@/providers/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,14 +16,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className='container'>
-          <div className='wrapper'>
-            <Navbar />
-            {children}
-            <Footer />
+        <ThemeContextProvider>
+          <ThemeProvider>
+            <div className='container'>
+              <div className='wrapper'>
+                <Navbar />
+                {children}
+                <Footer />
 
-          </div>
-        </div>
+              </div>
+            </div>
+          </ThemeProvider>
+        </ThemeContextProvider>
+
       </body>
     </html>
   )
