5db3ca9d4422bb19881d838068ef7a5010178d99
arunikax <srarfian@gmail.com>
2024-08-03 16:09:55 +0700

    feat:Category Component Design

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -13,7 +13,7 @@ const Featured = () => {
                 <Image src="/p1.jpeg" alt="" fill className={styles.image}/>
                 </div>
                 <div className={styles.textContainer}>
-                    <h1 className={styles.postTitle}> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas quo repellat itaque rerum laborum, placeat corrupti tenetur repellendus maiores accusamus velit fuga ipsa! Excepturi fuga ut consequuntur quos aspernatur quo!</h1>
+                    <h1 className={styles.postTitle}> Lorem, ipsum dolor sit amet consectetur adipisicing elit. </h1>
                     <p className={styles.postDesc}>
 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam praesentium architecto perspiciatis impedit. Quisquam maxime adipisci quae temporibus, itaque, consequatur minus nobis quas beatae ipsa fugiat iusto eaque, alias eius.
                     </p>
